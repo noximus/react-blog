f92bbb7a45b43dc3f74683946f7b795b9aac4387
Henry Richardson <hrichardson83@gmail.com>
2019-03-26 04:30:53 -0400

    async and thunk working & memoize

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -4,5 +4,5 @@ export default (state = [], action) => {
       return action.payload;
     default: 
       return state;
-  }
+  };
 };
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -2,11 +2,10 @@ const initialState = []
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-
-  case 'FETCH_USER':
-    return [...state, payload ]
-  default:
-    return state
-  }
-}
+    case 'FETCH_USER':
+      return [...state, payload ]
+    default:
+      return state
+  };
+};
 

bb42293ebb1b6fb5f294e7e6f71e0b5d01477d56
Henry Richardson <hrichardson83@gmail.com>
2019-03-26 04:02:41 -0400

    extracting logic to mapStateToProps

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,12 @@
+const initialState = []
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+
+  case 'FETCH_USER':
+    return [...state, payload ]
+  default:
+    return state
+  }
+}
+
